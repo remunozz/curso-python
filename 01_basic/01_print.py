1248d23d50a758535c0d0bbac6ed4f18130b40e3
midudev <miduga@gmail.com>
2025-01-15 18:50:58 +0100

    Add basic print examples in 01_print.py to demonstrate usage of the print() function in Python

---
+++ 01_basic/01_print.py
@@ -0,0 +1,16 @@
+###
+# 01 - print()
+# El módulo print() es el módulo que nos permite imprimir en consola
+# Sirve para mostrar información en consola y te va a acompañar
+# TODA TU VIDA. Desde hoy hasta el fin de los tiempos
+###
+
+print("¡Hola, Twitch!")
+print('Esto también funciona con una comilla')
+
+print("Python", "es", "genial")
+
+print("Python", "es", "brutal", sep = "-")
+
+print("Esto se imprime", end = "\n")
+print("en una línea")
